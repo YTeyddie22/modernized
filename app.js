1ed8982ea92bf20331fee41b5e4cfb27e9066493
Teyddie22 <teyddie@gmail.com>
2022-03-11 04:52:46 +0300

    Navigation

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const section1 = document.querySelector('#section__1');
 
 ///////////////////////////////////////////////////////////
 
-//! Navigation section
+//! Navigation section selector
 
 const nav = document.querySelector('.nav');
 
@@ -17,4 +17,19 @@ const nav = document.querySelector('.nav');
 //The Continue btn on click scrolls to the first section
 btnScrollTo.addEventListener('click', () => {
 	section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+//////////////////////////////////////////////////////////////
+
+//! Each Section Navigation
+
+document.querySelector('.nav__links').addEventListener('click', (e) => {
+	e.preventDefault();
+
+	const navLink = e.target.classList.contains('nav__link');
+
+	if (navLink) {
+		const id = e.target.getAttribute('href');
+		document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+	}
 });
